9e65ee18bb788faaf8b10220376ece363f3aaea5
fateful1 <facelessasha@yandex.ru>
2023-03-15 10:31:03 +0500

    emptying default project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,7 @@ import '../index.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main>
+     <Component {...pageProps} />
+   </main>
 }
